ec1b322fd6032ebe10c9229fa55b43a731d6a2c7
Akarachudra <kpytu3ha@yandex.ru>
2018-10-14 02:15:37 +0500

    Add propertyInfo patch

--- ObjectPatcher/Patcher.cs
+++ ObjectPatcher/Patcher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -6,16 +7,60 @@ namespace ObjectPatcher
 {
     public static class Patcher
     {
+        private static ConcurrentDictionary<Type, ConcurrentDictionary<string, MemberInfo>> typeMember;
+
+        static Patcher()
+        {
+            typeMember = new ConcurrentDictionary<Type, ConcurrentDictionary<string, MemberInfo>>();
+        }
+
         public static void Apply<T>(T obj, Dictionary<string, object> patchDictionary)
         {
+            var type = obj.GetType();
             foreach (var entry in patchDictionary)
             {
-                var propertyInfo = obj.GetType().GetRuntimeProperty(entry.Key);
-                if (propertyInfo == null)
+                var memberInfo = GetMemberInfo(type, entry.Key);
+                var propertyInfo = memberInfo as PropertyInfo;
+                if (propertyInfo != null)
                 {
-                    throw new MissingMemberException();
+                    propertyInfo.SetValue(obj, entry.Value);
                 }
             }
+        }
+
+        private static MemberInfo GetMemberInfo(Type t, string key)
+        {
+            if (!typeMember.TryGetValue(t, out var members))
+            {
+                FillMembersForType(t);
+                members = typeMember[t];
+            }
+
+            var normalizedKey = key.ToLower();
+            if (members.TryGetValue(normalizedKey, out var memberInfo))
+            {
+                return memberInfo;
+            }
+
+            return null;
+        }
+
+        private static void FillMembersForType(Type t)
+        {
+            var members = new ConcurrentDictionary<string, MemberInfo>();
+            var properties = t.GetRuntimeProperties();
+            var fields = t.GetRuntimeFields();
+            foreach (var propertyInfo in properties)
+            {
+                members.TryAdd(propertyInfo.Name.ToLower(), propertyInfo);
+            }
+
+            foreach (var fieldInfo in fields)
+            {
+                members.TryAdd(fieldInfo.Name.ToLower(), fieldInfo);
+            }
+
+            typeMember.TryAdd(t, members);
         }
     }
 }
